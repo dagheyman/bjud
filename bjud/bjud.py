bbbe92e2db1af79b3023fca39211079d5ce676c1
Dag Heyman <dag.heyman@gmail.com>
2024-02-23 21:13:26 +0100

    format with black

--- bjud/bjud.py
+++ bjud/bjud.py
@@ -2,6 +2,7 @@ import json
 from dataclasses import dataclass
 from playwright.sync_api import sync_playwright
 
+
 @dataclass
 class FbEvent:
     title: str
@@ -18,9 +19,9 @@ class Bjud:
     def get_event(self, url):
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
-            page = browser.new_page(locale='en-US')
+            page = browser.new_page(locale="en-US")
             page.goto(url)
-            
+
             try:
                 page.get_by_role("button", name="Allow all cookies").click(timeout=100)
             except Exception as e:
@@ -39,14 +40,19 @@ class Bjud:
             for l in page.locator("//body/script").all():
                 try:
                     d = json.loads(l.text_content())
-                    event = d['require'][0][3][0]['__bbox']['require'][7][3][1]['__bbox']['result']['data']['event']
-                    description = event['event_description']['text']
-                    location = event['location']['reverse_geocode']['city_page']['name']
-                    address = event['one_line_address']
-                    organiser = event['event_creator']['name']
-                    return FbEvent(title='TODO', description=description, organiser=organiser, address=address)
+                    event = d["require"][0][3][0]["__bbox"]["require"][7][3][1][
+                        "__bbox"
+                    ]["result"]["data"]["event"]
+                    description = event["event_description"]["text"]
+                    location = event["location"]["reverse_geocode"]["city_page"]["name"]
+                    address = event["one_line_address"]
+                    organiser = event["event_creator"]["name"]
+                    return FbEvent(
+                        title="TODO",
+                        description=description,
+                        organiser=organiser,
+                        address=address,
+                    )
                 except Exception as e:
                     pass
             browser.close()
-
-    

5396981a043fd20536d92c5e22a0ddddde695cbe
Dag Heyman <dag.heyman@gmail.com>
2024-02-23 21:09:51 +0100

    add first test

--- bjud/bjud.py
+++ bjud/bjud.py
@@ -1,28 +1,52 @@
 import json
+from dataclasses import dataclass
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.facebook.com/events/1828636217581951/")
-    #page.goto("https://www.facebook.com/events/317911824550706/")
-    page.get_by_role("button", name="Allow all cookies").click()
-    page.get_by_role("button", name="See more").first.click()
-    for l in page.locator("//body/script").all():
-        try:
-            d = json.loads(l.text_content())
-            event = d['require'][0][3][0]['__bbox']['require'][7][3][1]['__bbox']['result']['data']['event']
-            description = event['event_description']['text']
-            location = event['location']['reverse_geocode']['city_page']['name']
-            address = event['one_line_address']
-            organiser = event['event_creator']['name']
-            print(description)
-            print(location)
-            print(address)
-            print(organiser)
+@dataclass
+class FbEvent:
+    title: str
+    description: str
+    organiser: str
+    address: str
 
-        except Exception as e:
-            pass
-    browser.close()
+
+class Bjud:
+
+    def __init__(self):
+        pass
+
+    def get_event(self, url):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page(locale='en-US')
+            page.goto(url)
+            
+            try:
+                page.get_by_role("button", name="Allow all cookies").click(timeout=100)
+            except Exception as e:
+                pass
+
+            try:
+                page.get_by_label("Close").click(timeout=100)
+            except Exception as e:
+                pass
+
+            try:
+                page.get_by_role("button", name="See more").first.click(timeout=100)
+            except Exception as e:
+                pass
+
+            for l in page.locator("//body/script").all():
+                try:
+                    d = json.loads(l.text_content())
+                    event = d['require'][0][3][0]['__bbox']['require'][7][3][1]['__bbox']['result']['data']['event']
+                    description = event['event_description']['text']
+                    location = event['location']['reverse_geocode']['city_page']['name']
+                    address = event['one_line_address']
+                    organiser = event['event_creator']['name']
+                    return FbEvent(title='TODO', description=description, organiser=organiser, address=address)
+                except Exception as e:
+                    pass
+            browser.close()
 
     
---
+++ tests/test_bjud.py
@@ -0,0 +1,9 @@
+from bjud.bjud import Bjud
+
+def test_get_event():
+    bjud = Bjud()
+    e = bjud.get_event('https://www.facebook.com/events/415842204239647/')
+    assert 'Experimental' in e.description
+
+    e = bjud.get_event('https://www.facebook.com/events/1828636217581951')
+    assert 'Take Concrete Action' in e.organiser

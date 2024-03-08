2e4a4f4968cfc6036528748e3c382430358b031f
Dag Heyman <dag.heyman@gmail.com>
2024-03-08 21:25:53 +0100

    put get_evet_links function on same client
    - append /event in url

--- bjud/bjud.py
+++ bjud/bjud.py
@@ -22,6 +22,47 @@ class Bjud:
     def __init__(self):
         pass
 
+    def get_event_links(self, url):
+        events = []
+
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page(locale="en-US")
+            page.goto(url + "/events/")
+
+            try:
+                page.get_by_role("button", name="Allow all cookies").click(timeout=100)
+            except Exception as e:
+                pass
+
+            try:
+                page.get_by_label("Close").click(timeout=100)
+            except Exception as e:
+                pass
+
+            for l in page.locator("//body/script").all():
+                try:
+                    d = json.loads(l.text_content())
+                    data = d["require"][0][3][0]["__bbox"]["require"][5][3][1][
+                        "__bbox"
+                    ]["result"]["data"]["node"]["all_collections"]["nodes"][0][
+                        "style_renderer"
+                    ][
+                        "collection"
+                    ][
+                        "pageItems"
+                    ][
+                        "edges"
+                    ]
+                    for n in data:
+                        events.append(n["node"]["node"]["url"])
+                except Exception as e:
+                    pass
+
+            return events
+
+        raise ValueError("Loop failed")
+
     def get_event(self, url):
 
         # Default values
